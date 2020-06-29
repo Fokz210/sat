e5c70c100bd8b6a21242df2da94c6ac9099c11a7
Fokz210 <cheremisenov96@gmail.com>
2020-06-29 17:31:43 +0300

    update

--- js/SatteliteVisualizer.js
+++ js/SatteliteVisualizer.js
@@ -12,7 +12,10 @@ import
     TextureLoader,
     MeshLambertMaterial,
     Matrix4,
-    Vector3
+    Vector3,
+    Curve,
+    TorusBufferGeometry,
+    TubeBufferGeometry
 } from './three.js-master/build/three.module.js';
 import { OrbitControls } from './three.js-master/examples/jsm/controls/OrbitControls.js';
 import { STLLoader } from './three.js-master/examples/jsm/loaders/STLLoader.js';
@@ -35,7 +38,7 @@ export class SatteliteVisualizer
         this.canvasSizeY = sizeY;
 
         this.geostat = [];
-        this.satnames = 
+        this.geostatnames = 
         [
             'AMY1',
             'AM3', // AM1
@@ -48,6 +51,44 @@ export class SatteliteVisualizer
             'AM3'  // AM44 
         ];
 
+        this.lstat = [];
+        this.lstatnames = 
+        [
+            'AM3',
+            'AM3',
+            'AM3',
+            'AM3',
+        ];
+
+        this.lmeshes = [];
+        this.geostatmesh;
+
+        function Ellipse(xRadius, yRadius)
+        {
+			Curve.call( this );
+
+			this.xRadius = xRadius;
+			this.yRadius = yRadius;
+		}
+		
+		Ellipse.prototype = Object.create( Curve.prototype );
+		Ellipse.prototype.constructor = Ellipse;
+		
+        Ellipse.prototype.getPoint = function (t)
+        {
+		    var radians = 2 * Math.PI * t;
+		
+		    return new Vector3 (this.xRadius * Math.cos (radians), this.yRadius * Math.sin (radians), 0);
+		};
+
+        this.t = 0;
+
+        this.lmatrix = [];
+        this.lshift = [];
+
+        this.initlMatrix();
+     
+        this.lPath = new Ellipse (20, 50);
        
         this.node = document.getElementById ('container');
 
@@ -92,14 +133,65 @@ export class SatteliteVisualizer
         document.getElementById ("midSwitch").onclick = this.focusSat.bind(this);
         document.getElementById ("rightSwitch").onclick = this.focusGlobe.bind(this);
 
+        this.initPathMeshes();
+
         this.node.appendChild (this.renderer.domElement);
         this.animate();
+    }
+
+    initlMatrix ()
+    {
+        this.lmatrix.push (new Matrix4());
+        this.lmatrix[0].multiply (new Matrix4().makeRotationX (0.471239));
+        this.lshift.push (new Vector3 (0, 30, 0));
+        this.lshift[0].applyMatrix4 (this.lmatrix[0]);
+        this.lmatrix[0].setPosition (this.lshift[0]);
+
+        this.lmatrix.push (new Matrix4().makeRotationY(Math.PI / 2));
+        this.lmatrix[1].multiply (new Matrix4().makeRotationX (-0.471239));
+        this.lshift.push (new Vector3 (0, 30, 0));
+        this.lshift[1].applyMatrix4 (this.lmatrix[1]);
+        this.lmatrix[1].setPosition (this.lshift[1]);
+
+        this.lmatrix.push (new Matrix4());
+        this.lmatrix[2].multiply (new Matrix4().makeRotationX (-0.471239));
+        this.lshift.push (new Vector3 (0, 30, 0));
+        this.lshift[2].applyMatrix4 (this.lmatrix[2]);
+        this.lmatrix[2].setPosition (this.lshift[2]);
+        
+        this.lmatrix.push (new Matrix4().makeRotationY(Math.PI / 2));
+        this.lmatrix[3].multiply (new Matrix4().makeRotationX (0.471239));
+        this.lshift.push (new Vector3 (0, 30, 0));
+        this.lshift[3].applyMatrix4 (this.lmatrix[3]);
+        this.lmatrix[3].setPosition (this.lshift[3]);
+
     }
 
     animate ()
     {
         requestAnimationFrame (this.animate.bind(this));
         
+        if (this.satsLoaded)
+        {
+            for (let i = 0; i < this.lstat.length; i++)
+            {
+                var lt = (i % 2 == 0) ? this.t : this.t + 0.5;
+
+                if (lt > 1) lt -= 1;
+
+                var pt = this.lPath.getPoint (lt);
+                pt.applyMatrix4 (this.lmatrix[i]);
+                this.lstat[i].position.set (pt.x, pt.y, pt.z);
+
+                var up = new Vector3 (1, 0, 0);
+                up.applyMatrix4 (new Matrix4().makeRotationX (-0.471239));
+
+                this.meshLookAt (this.lstat[i], this.globeMesh, up);
+            }
+        
+            this.t = (this.t >= 1) ? 0 : this.t += 0.001;
+        }
+
         this.renderer.render (this.scene, this.camera);
     
         this.controls.update ();  
@@ -146,8 +238,14 @@ export class SatteliteVisualizer
             this.meshLookAt (this.geostat[i], this.globeMesh, new Vector3 (0, 0, 1));
         }
 
+        for (let i = 0; i < this.lstat.length; i++)
+            this.lstat[i].scale.set (0.1, 0.1, 0.1);
+
         for (let i = 0; i < this.geostat.length; i++)
             this.scene.add (this.geostat[i]);
+
+        for (let i = 0; i < this.lstat.length; i++)
+            this.scene.add (this.lstat[i]);
 
         this.satsLoaded = true;
 
@@ -155,11 +253,28 @@ export class SatteliteVisualizer
 
     loadSatsMeshes ()
     {
-        for (let i = 0; i < this.satnames.length; i++)
+        for (let i = 0; i < this.lstatnames.length; i++)
         {
             var that = this;
 
-            this.stlLoader.load ('/3d/' + this.satnames[i] + '.stl', 
+            this.stlLoader.load ('3d/' + this.lstatnames[i] + '.stl', 
+                function (geometry)
+                {
+                    var material = new MeshBasicMaterial ({ color: 0x737373 });
+                    var mesh = new Mesh (geometry, material);
+
+                    that.lstat.push(mesh);
+
+                    if (that.geostat.length == 9 && that.lstat.length == 8)
+                        that.initSats();
+                });
+        }
+
+        for (let i = 0; i < this.geostatnames.length; i++)
+        {
+            var that = this;
+
+            this.stlLoader.load ('3d/' + this.geostatnames[i] + '.stl', 
                 function (geometry)
                 {
                     var material = new MeshBasicMaterial ({ color: 0x737373 });
@@ -167,7 +282,7 @@ export class SatteliteVisualizer
 
                     that.geostat.push(mesh);
 
-                    if (that.geostat.length == 9)
+                    if (that.geostat.length == 9 && that.lstat.length == 4)
                         that.initSats();
                 });
         }
@@ -211,10 +326,42 @@ export class SatteliteVisualizer
         mesh.position.y = 0;
         mesh.position.z = 0;
 
+        mesh.rotation.set (0, 0, 0);
+
         mesh.applyMatrix4 (rot);
 
         mesh.position.x = pos.x;
         mesh.position.y = pos.y;
         mesh.position.z = pos.z;
+    }
+
+    initPathMeshes ()
+    {
+        this.geostatmesh = new Mesh 
+        (
+            new TorusBufferGeometry (46, 0.05, 16, 100),
+            new MeshBasicMaterial ({ color: 0xc9c9c9 })
+        );
+        this.geostatmesh.rotation.x = Math.PI / 2;
+        this.scene.add (this.geostatmesh);
+
+        this.lmeshes.push 
+        (
+            new Mesh 
+            (
+                new TubeBufferGeometry (this.lPath, 128, 0.05, 64, true),
+                new MeshBasicMaterial ({ color: 0xc9c9c9 })
+            )
+        );
+
+        this.lmeshes.push (this.lmeshes[0].clone());
+        this.lmeshes.push (this.lmeshes[0].clone());
+        this.lmeshes.push (this.lmeshes[0].clone());
+
+        for (let i = 0; i < this.lmeshes.length; i++)
+        {
+            this.lmeshes[i].applyMatrix4 (this.lmatrix[i]);
+            this.scene.add (this.lmeshes[i]);
+        }
     }
 };
