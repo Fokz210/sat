a8db5353db4a115c091c9aaa3e88b3c5c2a9e6b9
Fokz210 <cheremisenov96@gmail.com>
2020-07-18 23:04:26 +0300

    added textures

--- js/SatteliteVisualizer.js
+++ js/SatteliteVisualizer.js
@@ -783,7 +783,16 @@ export class SatteliteVisualizer
 
     loadBeams()
     {
-       
+        this.beams.push (new Beam ("none", "none", "none", "none", "./beams/AMU1_afr.png", "./beams/AMU1_rus.png", "none", "none", "./beams/AMU1_ka.png", "none", "./bands/AMU-Ku.png", "./bands/AMU-Ka.png"));  
+        this.beams.push (new Beam ("./beams/AM7_FC.png", "./beams/AM7_SC.png", "none", "none", "./beams/AM7_FK1.png", "./beams/AM7_FK2.png", "./beams/AM7_FK3.png", "./beams/AM7_SK.png", "none", "./bands/AM7-C.png", "./bands/AM7-Ku.png", "none"));
+        this.beams.push (new Beam ("./beams/AM6_FC1.png", "./beams/AM6_SC2.png", "none", "none", "./beams/AM6_FK1.png", "./beams/AM6_FK2.png", "./beams/AM6_STK1.png", "./beams/AM6_STK2.png", "./beams/AM6_KaMB.png", "./bandsAM6-C.png", "./bandsAM6-Ku.png", "none"));
+        this.beams.push (new Beam ("none", "none", "none", "none", "./beams/AT1_FK-east.png", "./beams/AT1_FK-wide.png", "none", "none", "none", "none", "none", "./bands/AT1-Ku.png", "none"));
+        this.beams.push (new Beam ("./beams/AM33_S1.png", "none", "none", "none", "./beams/AM33_ST1.png", "./beams/AM33_ST2.png", "none", "none", "none", "./bands/AM33-C.png", "none", "none"));
+        this.beams.push (new Beam ("./beams/AM3_FC.png", "./beams/AM3_SC.png", "none", "none", "./beams/AM3_FK.png", "./beams/AM3_SK.png", "none", "none", "none", "./bands/AM3-C copy.png", "./bands/AM3-Ku.png", "none"));
+        this.beams.push (new Beam ("./beams/AM5_FC.png", "./beams/AM5_SC.png", "none", "none", "./beams/AM5_FK1.png", "./beams/AM5_FK2.png", "none", "none", "none", "./bands/AM5-C.png", "./bands/AM5-Ku.png", "./bands/AM5-Ka.png"));
+        this.beams.push (new Beam ("none", "none", "none", "none", "./beams/AT2_FK-west.png", "none", "none", "none", "none", "none", "./bands/ATM2-Ku.png", "none"));
+        this.beams.push (new Beam ("./beams/AM8_FC1.png", "./beams/AM8_FC2.png", "none", "none", "./beams/AM8_FK1.png", "./beams/AM8_FK2.png", "./beams/AM8_FK3.png", "none", "none", "none", "./bands/AM8-Ku.png", "none"));
+        this.beams.push (new Beam ("./beams/AM44_S1.png", "none", "none", "none", "./beams/AM44_ST1.png", "./beams/AM44_ST2.png", "none", "none", "none", "./bands/AM44-C.png", "./bands/AM44-Ku.png", "none"));
     }
 
 };
